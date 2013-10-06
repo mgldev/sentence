21c9662879124e60e80790dde3f1e6e889aa47f6
Rick Ogden <rick@rickogden.com>
2013-10-06 11:04:50 +0100

    removed leftover code

--- src/Sentence/Ballsup/RepeatFirstOrLastLetter.php
+++ src/Sentence/Ballsup/RepeatFirstOrLastLetter.php
@@ -25,28 +25,3 @@ class RepeatFirstOrLastLetter implements Ballsup
 		return $retval;
 	}
 }
-
-
-$effect = mt_rand(1,3);
-$len = strlen($fragment);
-$retval = null;
-
-switch ($effect) {
-
-    case 1:
-        // swap 1st/2nd chars
-        if ($len > 1) {
-            $retval = $fragment[1] . $fragment[0] . substr($fragment, 2, $len - 2);
-        }
-        break;
-
-    case 2:
-        // make seemingly genuine keyboard mistakes
-
-        break;
-
-    default:
-        break;
-}
-
-return $retval;
